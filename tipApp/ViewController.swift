2070814a77d5522f2a10bac9445b19aa34420cf1
Feizza Fazilatun <feizza01@gmail.com>
2020-11-21 01:43:29 -0500

    calculateTip() added

--- tipApp/ViewController.swift
+++ tipApp/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,6 +20,20 @@ class ViewController: UIViewController {
     
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //Get bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        //Calculate tip amount
+        let tip = bill * 0.1
+        let total = bill + tip
+        
+        //Update tip and total labels
+        tipLabel.text = String(format: "$%.2f",  tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
 }
 
